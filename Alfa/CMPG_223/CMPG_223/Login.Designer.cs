374afaadbe974128f61b99de515d98747ad344c1
Cornelius Frylinck <27584046@student.g.nwu.ac.za>
2020-10-31 01:15:53 +0200

    login working

--- Alfa/CMPG_223/CMPG_223/Login.Designer.cs
+++ Alfa/CMPG_223/CMPG_223/Login.Designer.cs
@@ -31,8 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbxMail = new System.Windows.Forms.TextBox();
+            this.tbxPW = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -40,18 +40,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(273, 143);
+            this.label1.Location = new System.Drawing.Point(205, 116);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(270, 95);
+            this.label1.Size = new System.Drawing.Size(213, 76);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(181, 335);
+            this.label2.Location = new System.Drawing.Point(136, 272);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Email Address:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -59,47 +61,53 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(210, 386);
+            this.label3.Location = new System.Drawing.Point(158, 314);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Password:";
             // 
-            // textBox1
+            // tbxMail
             // 
-            this.textBox1.Location = new System.Drawing.Point(289, 332);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(215, 22);
-            this.textBox1.TabIndex = 3;
+            this.tbxMail.Location = new System.Drawing.Point(217, 270);
+            this.tbxMail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbxMail.Name = "tbxMail";
+            this.tbxMail.Size = new System.Drawing.Size(162, 20);
+            this.tbxMail.TabIndex = 3;
             // 
-            // textBox2
+            // tbxPW
             // 
-            this.textBox2.Location = new System.Drawing.Point(289, 381);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(215, 22);
-            this.textBox2.TabIndex = 4;
+            this.tbxPW.Location = new System.Drawing.Point(217, 310);
+            this.tbxPW.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbxPW.Name = "tbxPW";
+            this.tbxPW.PasswordChar = '*';
+            this.tbxPW.Size = new System.Drawing.Size(162, 20);
+            this.tbxPW.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 445);
+            this.button1.Location = new System.Drawing.Point(217, 362);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(215, 64);
+            this.button1.Size = new System.Drawing.Size(161, 52);
             this.button1.TabIndex = 5;
             this.button1.Text = "Log In to the sysytem";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 751);
+            this.ClientSize = new System.Drawing.Size(600, 609);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbxPW);
+            this.Controls.Add(this.tbxMail);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
@@ -114,8 +122,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbxMail;
+        private System.Windows.Forms.TextBox tbxPW;
         private System.Windows.Forms.Button button1;
     }
 }
--- Alfa/CMPG_223/CMPG_223/Login.cs
+++ Alfa/CMPG_223/CMPG_223/Login.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Net.Sockets;
 
 namespace CMPG_223
 {
     public partial class Login : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Dev\\GitHub\\CMPG223_Project_2020-reinhardt\\Alfa\\CMPG_223\\CMPG_223\\Spec1.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
         public Login()
         {
             InitializeComponent();
@@ -25,6 +32,60 @@ namespace CMPG_223
         private void Login_Load(object sender, EventArgs e)
         {
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            //check valid details
+            if(!IsValidMail(tbxMail.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("Please enter a correct email");
+                con.Close();
+                return;
+            }
+
+            //log in
+            cmd = new SqlCommand("SELECT * FROM LOGIN_DETAILS WHERE Email = @mail", con);
+            cmd.Parameters.Add(new SqlParameter("mail", tbxMail.Text));
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            // - check if user exists
+            try
+            {
+                // - check if password matches
+                if (!(tbxPW.Text == dr[1].ToString()))
+                {
+                    System.Windows.Forms.MessageBox.Show("Incorrect password.");
+                    con.Close();
+                    return;
+                }
+            } catch(Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("User does not exist.");
+                Debug.WriteLine(ex.Message);
+                con.Close();
+                return;
+            }
+
+            con.Close();
+
+            Orders form2 = new Orders();
+            form2.MdiParent = this;
+            form2.Show();
+        }
+
+        bool IsValidMail (String mail) // check if valid email
+        {
+            try
+            {
+                var ml = new System.Net.Mail.MailAddress(mail);
+                return ml.Address == mail;
+            }catch
+            {
+                return false;
+            }
         }
     }
 }
